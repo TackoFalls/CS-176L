33483f472d4ffb3206d8fe918f8837eebe81e16e
TackoFalls <69122331+TackoFalls@users.noreply.github.com>
2022-02-16 14:15:27 -0500

    Microwave Assignment(Final)
    Final and updated version of the microwave assignment.

--- microwave.java
+++ microwave.java
@@ -1,9 +1,53 @@
 
 public class microwave {
 
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Instant Variables 
+		
+	private int time;
+	private int power;
+		
 
+		
+		//Constructor goes here
+		public microwave() {
+			
+			time = 0;
+			power = 1;
+		}
+		
+		//timeButton goes here 
+		public void timeButton() {
+			
+			time = time + 30;
+			
+		}
+		//powerButton goes here 
+		public void powerButton() {
+			
+			if (power == 1)
+			{power = 2;}
+
+			else {power = 1;}
+		}
+
+		//resetButton goes here 
+	public void resetButton() {
+		
+		time = 0;
+		power = 1;
+		
+	    }
+
+		//startButton goes here 
+	public void startButton() {
+		
+		System.out.println("Cooking for " + (time) + " seconds at level " + (power) + ("."));
+		
 	}
+		
+		
+	
 
 }
--- microwaveTester.java
+++ microwaveTester.java
@@ -2,8 +2,25 @@
 public class microwaveTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    //Create a Microwave object
+		microwave myMicrowave = new microwave();
+		
+	//Hit the time button
+		myMicrowave.timeButton();
+		
+	//Hit the power button
+		myMicrowave.powerButton();
 
+	
+	//Hit the start button
+		myMicrowave.startButton();
+
+		
+	//Hit the reset button
+		myMicrowave.resetButton();
+
+		
+		
 	}
 
 }
