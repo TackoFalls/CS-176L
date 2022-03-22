8af0d2663bf451edda5939e780923e3af5f720de
TackoFalls <69122331+TackoFalls@users.noreply.github.com>
2022-03-21 22:42:45 -0400

    Array Assigment
    Not complete in 100% correct compution.

---
+++ ArrayAssignment1/src/array.java
@@ -0,0 +1,56 @@
+
+public class array {
+
+	public static boolean main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double[] values = {25.3, 100, -10, -1.5, 13, 98.6, 17, 123.145, 125.6, 123.146};
+		double target;
+		double total;
+
+
+		for(double x: values)
+			System.out.print(x + " ");
+
+			double max = values[0];
+			for(double s : values)
+			if(s > max) max = s;
+			System.out.println("The maximum element is " + max);
+
+			int count = 0;
+			for(double s: values)
+			if(s < 0) count++;
+			System.out.println("The negative numbers are " + count);
+
+			for(int i = 0; i < values.length; i++){
+			total = total + values[i];
+			}
+
+			for(int i = 0; i < values.length; i++){
+			   if(values[i] == target){
+			       return true;}
+			}
+
+
+			for(int i = 0; i < values.length; i++){
+			       values[i] = 2 * values[i];
+			}
+
+
+			for (double s: values){
+			total = total + s;
+			}
+
+			for (double s: values){
+			total = total + s;
+			} total = total - values[0];
+
+			int i = 0;
+			for (double s: values){
+			   if (s == target) {
+			       return i;}
+			   i++;
+			}
+		
+	}
+	}
