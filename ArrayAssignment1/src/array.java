d182d894d2c10b949b829c5f21f3add7a3ccdc0e
TackoFalls <69122331+TackoFalls@users.noreply.github.com>
2022-03-29 13:17:30 -0400

    Completed version of Array Assignment 1 3/29/22

--- ArrayAssignment1/src/array.java
+++ ArrayAssignment1/src/array.java
@@ -1,7 +1,7 @@
 
 public class array {
 
-	public static boolean main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		double[] values = {25.3, 100, -10, -1.5, 13, 98.6, 17, 123.145, 125.6, 123.146};
@@ -11,46 +11,76 @@ public class array {
 
 		for(double x: values)
 			System.out.print(x + " ");
-
-			double max = values[0];
-			for(double s : values)
-			if(s > max) max = s;
-			System.out.println("The maximum element is " + max);
-
-			int count = 0;
-			for(double s: values)
-			if(s < 0) count++;
-			System.out.println("The negative numbers are " + count);
-
-			for(int i = 0; i < values.length; i++){
-			total = total + values[i];
-			}
-
-			for(int i = 0; i < values.length; i++){
-			   if(values[i] == target){
-			       return true;}
-			}
-
-
-			for(int i = 0; i < values.length; i++){
-			       values[i] = 2 * values[i];
-			}
-
-
-			for (double s: values){
-			total = total + s;
-			}
-
-			for (double s: values){
-			total = total + s;
-			} total = total - values[0];
-
-			int i = 0;
-			for (double s: values){
-			   if (s == target) {
-			       return i;}
-			   i++;
-			}
 		
+		System.out.println(" ");
+
+	    double max = values[0];
+	    for(double s : values) 
+	    {
+		if(s > max) max = s;
+	    }
+		System.out.println("The maximum element is " + max);
+		int count = 0;
+		for(double n: values) {
+			if (n < 0) count = count + 1;
+			
+		}
+		System.out.println("There are " + (count) + " negative numbers within the array.");
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+//	    int count = 0;
+//			for(double s: values)
+//			if(s < 0) count++;
+//			System.out.println("The negative numbers are " + count);
+//
+//			for(int i = 0; i < values.length; i++){
+//			total = total + values[i];
+//			}
+//
+//			for(int i = 0; i < values.length; i++){
+//			   if(values[i] == target){
+//			       return true;}
+//			}
+//
+//
+//			for(int i = 0; i < values.length; i++){
+//			       values[i] = 2 * values[i];
+//			}
+//
+//
+//			for (double s: values){
+//			total = total + s;
+//			}
+//
+//			for (double s: values){
+//			total = total + s;
+//			} total = total - values[0];
+//
+//			int i = 0;
+//			for (double s: values){
+//			   if (s == target) {
+//			       }
+//			   i++;
+//			}
+//		
 	}
-	}
+}
