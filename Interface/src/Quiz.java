13b5eb1764c8491a607ccbf94c8ea47ce216b8d1
TackoFalls <69122331+TackoFalls@users.noreply.github.com>
2022-04-14 14:09:16 -0400

    Interface Assignment(Quiz edition)

---
+++ Interface/src/Quiz.java
@@ -0,0 +1,14 @@
+
+public class Quiz implements Measurable {
+	
+private double score;
+public Quiz(double score) {
+	
+	this.score = score;
+	
+} public double getMeasure()
+{
+   return score;
+}
+
+}
---
+++ Interface/src/Viewer.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionListener;
+public class Viewer {
+
+	
+	private static final int FRAME_WIDTH = 240;
+	private static final int FRAME_HEIGHT = 240;
+	
+	public static void main (String[] args) {
+		
+		
+		
+		
+	}
+	
+}
